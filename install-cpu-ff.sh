561e1208eb05c0b5e3d5215996206c40d60c5462
ChongFF <fanfei.chong@gmail.com>
2018-08-13 17:49:46 -0400

    Add installing r to CPU VM set-up

--- install-cpu-ff.sh
+++ install-cpu-ff.sh
@@ -82,4 +82,4 @@ echo "If you get an error instead, try restarting your session so your $PATH is 
 
 # Installing commonly used packages
 pip install imagehash hyperopt
-conda install -y py-xgboost matplotlib seaborn
+conda install -y py-xgboost matplotlib seaborn r r-essentials
