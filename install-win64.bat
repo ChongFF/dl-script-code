e9199e5b1ca8801eb0d86a8db0b59f864ac45b60
ChongFF <fanfei.chong@gmail.com>
2018-09-12 23:47:50 -0400

    Update install-win64.bat

--- install-win64.bat
+++ install-win64.bat
@@ -34,7 +34,7 @@ ECHO # https://stackoverflow.com/questions/32389599/anaconda-importerror-libsm-s
 
 ECHO #### the following two lines are to force 3.4.3 instead of 3.5 of R is installed 
 conda install -y mro-base=3.4.3
-conda install -y -c r r-base64enc=0.1_3=mro343h086d26f_0
+conda install -y -c r r-base64enc=0.1_3=mro343h889e2dd_0
 ECHO #### the above two lines are to force 3.4.3 instead of 3.5 of R is installed 
 ECHO # choose R 3.4.3 because WeightedROC seems to be not available for R 3.5, and get lightGBM
 conda install -y -c r r-essentials r-rms
